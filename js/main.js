8ecb6c7b8a27e3154d75cbdd50d160dd2c8bd512
EstherMendozaM <117119934+EstherMendozaM@users.noreply.github.com>
2022-11-04 05:21:18 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,143 @@
+(function ($) {
+    "use strict";
+
+    // Spinner
+    var spinner = function () {
+        setTimeout(function () {
+            if ($('#spinner').length > 0) {
+                $('#spinner').removeClass('show');
+            }
+        }, 1);
+    };
+    spinner();
+    
+    
+    // Initiate the wowjs
+    new WOW().init();
+
+
+    // Navbar on scrolling
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.navbar').fadeIn('slow').css('display', 'flex');
+        } else {
+            $('.navbar').fadeOut('slow').css('display', 'none');
+        }
+    });
+
+
+    // Modal Video
+    var $videoSrc;
+    $('.btn-play').click(function () {
+        $videoSrc = $(this).data("src");
+    });
+    console.log($videoSrc);
+    $('#videoModal').on('shown.bs.modal', function (e) {
+        $("#video").attr('src', $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0");
+    })
+    $('#videoModal').on('hide.bs.modal', function (e) {
+        $("#video").attr('src', $videoSrc);
+    })
+    
+    
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+
+    // Facts counter
+    $('[data-toggle="counter-up"]').counterUp({
+        delay: 10,
+        time: 2000
+    });
+
+
+    // Testimonials carousel
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: false,
+        smartSpeed: 1000,
+        margin: 25,
+        loop: true,
+        center: true,
+        dots: false,
+        nav: true,
+        navText : [
+            '<i class="bi bi-chevron-left"></i>',
+            '<i class="bi bi-chevron-right"></i>'
+        ],
+        responsive: {
+            0:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:3
+            }
+        }
+    });
+
+    
+})(jQuery);
+
+
+
+  /**
+   * Scrolls to an element with header offset
+   */
+   const scrollto = (el) => {
+    let header = select('#header')
+    let offset = header.offsetHeight
+
+    if (!header.classList.contains('header-scrolled')) {
+      offset -= 20
+    }
+
+    let elementPos = select(el).offsetTop
+    window.scrollTo({
+      top: elementPos - offset,
+      behavior: 'smooth'
+    })
+  }
+
+    /**
+   * Toggle .header-scrolled class to #header when page is scrolled
+   */
+     let selectHeader = select('#header')
+     if (selectHeader) {
+       const headerScrolled = () => {
+         if (window.scrollY > 100) {
+           selectHeader.classList.add('header-scrolled')
+         } else {
+           selectHeader.classList.remove('header-scrolled')
+         }
+       }
+       window.addEventListener('load', headerScrolled)
+       onscroll(document, headerScrolled)
+     }
+   
+     /**
+      * Back to top button
+      */
+     let backtotop = select('.back-to-top')
+     if (backtotop) {
+       const toggleBacktotop = () => {
+         if (window.scrollY > 100) {
+           backtotop.classList.add('active')
+         } else {
+           backtotop.classList.remove('active')
+         }
+       }
+       window.addEventListener('load', toggleBacktotop)
+       onscroll(document, toggleBacktotop)
+     }
